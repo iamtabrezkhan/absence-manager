c86e97100ba7436cdefe810069ecc5b619713402
Tabrez Khan <khan.t.29@gmail.com>
2022-08-27 21:18:25 +0530

    chore: setup redux basic structure

---
+++ src/redux/appSlice.js
@@ -0,0 +1,9 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+export const counterSlice = createSlice({
+  name: "app",
+  initialState: {},
+  reducers: {},
+});
+
+export default counterSlice.reducer;
